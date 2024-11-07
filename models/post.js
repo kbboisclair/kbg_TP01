9484b68d7392788664b69ed02947e5ec53d14144
Problem7451 <kbboisclair@protonmail.com>
2024-11-07 15:38:45 -0500

    update

---
+++ models/post.js
@@ -0,0 +1,15 @@
+import Model from './model.js';
+
+export default class Post extends Model {
+    constructor() {
+        super(true /* secured Id */);
+
+        this.addField('Title', 'string');
+        this.addField('Text', 'string');
+        this.addField('Category', 'string');
+        this.addField('Image', 'asset');    // vérifier asset ds demo
+        this.addField('Creation', 'integer');
+
+        this.setKey("Title");
+    }
+}
---
+++ wwwroot/Posts/js/appUI.js
@@ -0,0 +1,360 @@
+const periodicRefreshPeriod = 10;
+let categories = [];
+let selectedCategory = "";
+let currentETag = "";
+let hold_Periodic_Refresh = false;
+let pageManager;
+let itemLayout;
+
+let waiting = null;
+let waitingGifTrigger = 2000;
+function addWaitingGif() {
+    clearTimeout(waiting);
+    waiting = setTimeout(() => {
+        $("#itemsPanel").append($("<div id='waitingGif' class='waitingGifcontainer'><img class='waitingGif' src='Loading_icon.gif' /></div>'"));
+    }, waitingGifTrigger)
+}
+function removeWaitingGif() {
+    clearTimeout(waiting);
+    $("#waitingGif").remove();
+}
+
+Init_UI();
+
+async function Init_UI() {
+    itemLayout = {
+        width: $("#sample").outerWidth(),
+        height: $("#sample").outerHeight()
+    };
+    pageManager = new PageManager('scrollPanel', 'itemsPanel', itemLayout, renderPosts);
+    compileCategories();
+    $('#createPost').on("click", async function () {
+        renderCreatePostForm();
+    });
+    $('#abort').on("click", async function () {
+        showPosts()
+    });
+    $('#aboutCmd').on("click", function () {
+        renderAbout();
+    });
+    showPosts();
+    start_Periodic_Refresh();
+}
+
+function showPosts() {  // a revoir
+    $("#actionTitle").text("Liste des favoris");
+    $("#scrollPanel").show();
+    $('#abort').hide();
+    $('#bookmarkForm').hide();
+    $('#aboutContainer').hide();
+    $("#createBookmark").show();
+    hold_Periodic_Refresh = false;
+}
+function hidePosts() {
+    $("#scrollPanel").hide();
+    $("#createPost").hide();
+    $("#abort").show();
+    hold_Periodic_Refresh = true;
+}
+function start_Periodic_Refresh() {
+    setInterval(async () => {
+        if (!hold_Periodic_Refresh) {
+            let etag = await Posts_API.HEAD();
+            if (currentETag != etag) {
+                currentETag = etag;
+                await pageManager.update(false);
+                compileCategories();
+            }
+        }
+    },
+        periodicRefreshPeriod * 1000);
+}
+function renderAbout() {
+    hidePosts();
+    $("#actionTitle").text("À propos...");
+    $("#aboutContainer").show();
+}
+function updateDropDownMenu() {
+    let DDMenu = $("#DDMenu");
+    let selectClass = selectedCategory === "" ? "fa-check" : "fa-fw";
+    DDMenu.empty();
+    DDMenu.append($(`
+        <div class="dropdown-item menuItemLayout" id="allCatCmd">
+            <i class="menuIcon fa ${selectClass} mx-2"></i> Toutes les catégories
+        </div>
+        `));
+    DDMenu.append($(`<div class="dropdown-divider"></div>`));
+    categories.forEach(category => {
+        selectClass = selectedCategory === category ? "fa-check" : "fa-fw";
+        DDMenu.append($(`
+            <div class="dropdown-item menuItemLayout category" id="allCatCmd">
+                <i class="menuIcon fa ${selectClass} mx-2"></i> ${category}
+            </div>
+        `));
+    })
+    DDMenu.append($(`<div class="dropdown-divider"></div> `));      
+    DDMenu.append($(`
+        <div class="dropdown-item menuItemLayout" id="aboutCmd">
+            <i class="menuIcon fa fa-info-circle mx-2"></i> À propos...
+        </div>
+        `));
+    $('#aboutCmd').on("click", function () {
+        renderAbout();
+    });
+    $('#allCatCmd').on("click", function () {
+        showPosts();
+        selectedCategory = "";
+        updateDropDownMenu();
+        pageManager.reset();
+    });
+    $('.category').on("click", function () {
+        showPosts();
+        selectedCategory = $(this).text().trim();
+        updateDropDownMenu();
+        pageManager.reset();
+    });
+}
+async function compileCategories() {
+    categories = [];
+    let response = await Posts_API.GetQuery("?fields=category&sort=category");
+    if (!Posts_API.error) {
+        let items = response.data;
+        if (items != null) {
+            items.forEach(item => {
+                if (!categories.includes(item.Category))
+                    categories.push(item.Category);
+            })
+            updateDropDownMenu(categories);
+        }
+    }
+}
+async function renderPosts(queryString) {
+    let endOfData = false;
+    queryString += "&sort=category";
+    if (selectedCategory != "") queryString += "&category=" + selectedCategory;
+    addWaitingGif();
+    let response = await Posts_API.Get(queryString);
+    if (!Posts_API.error) {
+        currentETag = response.ETag;
+        let Posts = response.data;
+        if (Posts.length > 0) {
+            Posts.forEach(Post => {
+                $("#itemsPanel").append(renderBookmark(Post));
+            });
+            $(".editCmd").off();
+            $(".editCmd").on("click", function () {
+                renderEditBookmarkForm($(this).attr("editBookmarkId"));
+            });
+            $(".deleteCmd").off();
+            $(".deleteCmd").on("click", function () {
+                renderDeleteBookmarkForm($(this).attr("deleteBookmarkId"));
+            });
+        } else
+            endOfData = true;
+    } else {
+        renderError(Posts_API.currentHttpError);
+    }
+    removeWaitingGif();
+    return endOfData;
+}
+
+function renderError(message) {
+    hidePosts();
+    $("#actionTitle").text("Erreur du serveur...");
+    $("#errorContainer").show();
+    $("#errorContainer").append($(`<div>${message}</div>`));
+}
+function renderCreatePostForm() {
+    renderPostForm();
+}
+async function renderEditPostForm(id) {
+    addWaitingGif();
+    let response = await Posts_API.Get(id)
+    if (!Posts_API.error) {
+        let Post = response.data;
+        if (Post !== null)
+            renderPostForm(Post);
+        else
+            renderError("Post introuvable!");
+    } else {
+        renderError(Posts_API.currentHttpError);
+    }
+    removeWaitingGif();
+}
+async function renderDeletePostForm(id) {
+    hidePosts();
+    $("#actionTitle").text("Retrait");
+    $('#postForm').show();
+    $('#postForm').empty();
+    let response = await Posts_API.Get(id)
+    if (!Posts_API.error) {
+        let Post = response.data;
+        let favicon = makeFavicon(Post.Url);
+        if (Post !== null) {
+            $("#PostForm").append(`
+        <div class="PostdeleteForm">
+            <h4>Effacer le favori suivant?</h4>
+            <br>
+            <div class="PostRow" id=${Bookmark.Id}">
+                <div class="PostContainer noselect">
+                    <div class="PostLayout">
+                        <div class="Post">
+                            <a href="${Post.Url}" target="_blank"> ${favicon} </a>
+                            <span class="PostTitle">${Post.Title}</span>
+                        </div>
+                        <span class="PostCategory">${Post.Category}</span>
+                    </div>
+                    <div class="PostCommandPanel">
+                        <span class="editCmd cmdIcon fa fa-pencil" editPostId="${Post.Id}" title="Modifier ${Post.Title}"></span>
+                        <span class="deleteCmd cmdIcon fa fa-trash" deletePostId="${Post.Id}" title="Effacer ${Post.Title}"></span>
+                    </div>
+                </div>
+            </div>   
+            <br>
+            <input type="button" value="Effacer" id="deletePost" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </div>    
+        `);
+            $('#deletePost').on("click", async function () {
+                await Posts_API.Delete(Post.Id);
+                if (!Posts_API.error) {
+                    showPosts();
+                    await pageManager.update(false);
+                    compileCategories();
+                }
+                else {
+                    console.log(Posts_API.currentHttpError)
+                    renderError("Une erreur est survenue!");
+                }
+            });
+            $('#cancel').on("click", function () { // ici
+                showPosts();
+            });
+
+        } else {
+            renderError("Bookmark introuvable!");
+        }
+    } else
+        renderError(Bookmarks_API.currentHttpError);
+}
+function getFormData($form) {
+    const removeTag = new RegExp("(<[a-zA-Z0-9]+>)|(</[a-zA-Z0-9]+>)", "g");
+    var jsonObject = {};
+    $.each($form.serializeArray(), (index, control) => {
+        jsonObject[control.name] = control.value.replace(removeTag, "");
+    });
+    return jsonObject;
+}
+function newBookmark() {
+    Bookmark = {};
+    Bookmark.Id = 0;
+    Bookmark.Title = "";
+    Bookmark.Url = "";
+    Bookmark.Category = "";
+    return Bookmark;
+}
+function renderBookmarkForm(Bookmark = null) {
+    hideBookmarks();
+    let create = Bookmark == null;
+    let favicon = `<div class="big-favicon"></div>`;
+    if (create)
+        Bookmark = newBookmark();
+    else
+        favicon = makeFavicon(Bookmark.Url, true);
+    $("#actionTitle").text(create ? "Création" : "Modification");
+    $("#bookmarkForm").show();
+    $("#bookmarkForm").empty();
+    $("#bookmarkForm").append(`
+        <form class="form" id="BookmarkForm">
+            <a href="${Bookmark.Url}" target="_blank" id="faviconLink" class="big-favicon" > ${favicon} </a>
+            <br>
+            <input type="hidden" name="Id" value="${Bookmark.Id}"/>
+
+            <label for="Title" class="form-label">Titre </label>
+            <input 
+                class="form-control Alpha"
+                name="Title" 
+                id="Title" 
+                placeholder="Titre"
+                required
+                RequireMessage="Veuillez entrer un titre"
+                InvalidMessage="Le titre comporte un caractère illégal"
+                value="${Bookmark.Title}"
+            />
+            <label for="Url" class="form-label">Url </label>
+            <input
+                class="form-control URL"
+                name="Url"
+                id="Url"
+                placeholder="Url"
+                required
+                value="${Bookmark.Url}" 
+            />
+            <label for="Category" class="form-label">Catégorie </label>
+            <input 
+                class="form-control"
+                name="Category"
+                id="Category"
+                placeholder="Catégorie"
+                required
+                value="${Bookmark.Category}"
+            />
+            <br>
+            <input type="submit" value="Enregistrer" id="saveBookmark" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </form>
+    `);
+    initFormValidation();
+    $("#Url").on("change", function () {
+        let favicon = makeFavicon($("#Url").val(), true);
+        $("#faviconLink").empty();
+        $("#faviconLink").attr("href", $("#Url").val());
+        $("#faviconLink").append(favicon);
+    })
+    $('#BookmarkForm').on("submit", async function (event) {
+        event.preventDefault();
+        let Bookmark = getFormData($("#BookmarkForm"));
+        Bookmark = await Bookmarks_API.Save(Bookmark, create);
+        if (!Bookmarks_API.error) {
+            showBookmarks();
+            await pageManager.update(false);
+            compileCategories();
+            pageManager.scrollToElem(Bookmark.Id);
+        }
+        else
+            renderError("Une erreur est survenue!");
+    });
+    $('#cancel').on("click", function () {
+        showBookmarks();
+    });
+}
+function makeFavicon(url, big = false) {
+    // Utiliser l'API de google pour extraire le favicon du site pointé par url
+    // retourne un élément div comportant le favicon en tant qu'image de fond
+    ///////////////////////////////////////////////////////////////////////////
+    if (url.slice(-1) != "/") url += "/";
+    let faviconClass = "favicon";
+    if (big) faviconClass = "big-favicon";
+    url = "http://www.google.com/s2/favicons?sz=64&domain=" + url;
+    return `<div class="${faviconClass}" style="background-image: url('${url}');"></div>`;
+}
+function renderBookmark(Bookmark) {
+    let favicon = makeFavicon(Bookmark.Url);
+    return $(`
+     <div class="BookmarkRow" id='${Bookmark.Id}'>
+        <div class="BookmarkContainer noselect">
+            <div class="BookmarkLayout">
+                <div class="Bookmark">
+                    <a href="${Bookmark.Url}" target="_blank"> ${favicon} </a>
+                    <span class="BookmarkTitle">${Bookmark.Title}</span>
+                </div>
+                <span class="BookmarkCategory">${Bookmark.Category}</span>
+            </div>
+            <div class="BookmarkCommandPanel">
+                <span class="editCmd cmdIcon fa fa-pencil" editBookmarkId="${Bookmark.Id}" title="Modifier ${Bookmark.Title}"></span>
+                <span class="deleteCmd cmdIcon fa fa-trash" deleteBookmarkId="${Bookmark.Id}" title="Effacer ${Bookmark.Title}"></span>
+            </div>
+        </div>
+    </div>           
+    `);
+}

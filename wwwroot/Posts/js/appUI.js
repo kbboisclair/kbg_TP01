a07fc56a0cdc10766a20d886b6f10efcd97f3512
Problem7451 <kbboisclair@protonmail.com>
2024-11-07 15:53:04 -0500

    Update appUI.js

--- wwwroot/Posts/js/appUI.js
+++ wwwroot/Posts/js/appUI.js
@@ -232,10 +232,10 @@ async function renderDeletePostForm(id) {
             });
 
         } else {
-            renderError("Bookmark introuvable!");
+            renderError("Post introuvable!");
         }
     } else
-        renderError(Bookmarks_API.currentHttpError);
+        renderError(Posts_API.currentHttpError);
 }
 function getFormData($form) {
     const removeTag = new RegExp("(<[a-zA-Z0-9]+>)|(</[a-zA-Z0-9]+>)", "g");
@@ -245,65 +245,67 @@ function getFormData($form) {
     });
     return jsonObject;
 }
-function newBookmark() {
-    Bookmark = {};
-    Bookmark.Id = 0;
-    Bookmark.Title = "";
-    Bookmark.Url = "";
-    Bookmark.Category = "";
-    return Bookmark;
+function newPost() {
+    Post = {};
+    Post.Id = 0;
+    Post.Title = "";
+    Post.Text = "";
+    Post.Category = "";
+    Post.Image = "";        // asset image à compléter
+    Post.Creation = 0;
+    return Post;
 }
-function renderBookmarkForm(Bookmark = null) {
-    hideBookmarks();
-    let create = Bookmark == null;
+function renderPostForm(Post = null) {
+    hidePosts();
+    let create = Post == null;
     let favicon = `<div class="big-favicon"></div>`;
     if (create)
-        Bookmark = newBookmark();
+        Post = newPost();
     else
-        favicon = makeFavicon(Bookmark.Url, true);
+        favicon = makeFavicon(Post.Url, true);
     $("#actionTitle").text(create ? "Création" : "Modification");
-    $("#bookmarkForm").show();
-    $("#bookmarkForm").empty();
-    $("#bookmarkForm").append(`
-        <form class="form" id="BookmarkForm">
-            <a href="${Bookmark.Url}" target="_blank" id="faviconLink" class="big-favicon" > ${favicon} </a>
-            <br>
-            <input type="hidden" name="Id" value="${Bookmark.Id}"/>
+    $("#PostForm").show();
+    $("#PostForm").empty();
+    // $("#PostForm").append(`
+    //     <form class="form" id="PostForm">
+    //         <a href="${Post.Url}" target="_blank" id="faviconLink" class="big-favicon" > ${favicon} </a>
+    //         <br>
+    //         <input type="hidden" name="Id" value="${Post.Id}"/>
 
-            <label for="Title" class="form-label">Titre </label>
-            <input 
-                class="form-control Alpha"
-                name="Title" 
-                id="Title" 
-                placeholder="Titre"
-                required
-                RequireMessage="Veuillez entrer un titre"
-                InvalidMessage="Le titre comporte un caractère illégal"
-                value="${Bookmark.Title}"
-            />
-            <label for="Url" class="form-label">Url </label>
-            <input
-                class="form-control URL"
-                name="Url"
-                id="Url"
-                placeholder="Url"
-                required
-                value="${Bookmark.Url}" 
-            />
-            <label for="Category" class="form-label">Catégorie </label>
-            <input 
-                class="form-control"
-                name="Category"
-                id="Category"
-                placeholder="Catégorie"
-                required
-                value="${Bookmark.Category}"
-            />
-            <br>
-            <input type="submit" value="Enregistrer" id="saveBookmark" class="btn btn-primary">
-            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
-        </form>
-    `);
+    //         <label for="Title" class="form-label">Titre </label>
+    //         <input 
+    //             class="form-control Alpha"
+    //             name="Title" 
+    //             id="Title" 
+    //             placeholder="Titre"
+    //             required
+    //             RequireMessage="Veuillez entrer un titre"
+    //             InvalidMessage="Le titre comporte un caractère illégal"
+    //             value="${Post.Title}"
+    //         />
+    //         <label for="Url" class="form-label">Url </label>
+    //         <input
+    //             class="form-control URL"
+    //             name="Url"
+    //             id="Url"
+    //             placeholder="Url"
+    //             required
+    //             value="${Post.Url}" 
+    //         />
+    //         <label for="Category" class="form-label">Catégorie </label>
+    //         <input 
+    //             class="form-control"
+    //             name="Category"
+    //             id="Category"
+    //             placeholder="Catégorie"
+    //             required
+    //             value="${Post.Category}"
+    //         />
+    //         <br>
+    //         <input type="submit" value="Enregistrer" id="saveBookmark" class="btn btn-primary">
+    //         <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+    //     </form>
+    // `);
     initFormValidation();
     $("#Url").on("change", function () {
         let favicon = makeFavicon($("#Url").val(), true);
@@ -311,21 +313,21 @@ function renderBookmarkForm(Bookmark = null) {
         $("#faviconLink").attr("href", $("#Url").val());
         $("#faviconLink").append(favicon);
     })
-    $('#BookmarkForm').on("submit", async function (event) {
+    $('#Post').on("submit", async function (event) {
         event.preventDefault();
-        let Bookmark = getFormData($("#BookmarkForm"));
-        Bookmark = await Bookmarks_API.Save(Bookmark, create);
-        if (!Bookmarks_API.error) {
-            showBookmarks();
+        let Post = getFormData($("#PostForm"));
+        Post = await Posts_API.Save(Post, create);
+        if (!Posts_API.error) {
+            showPosts();
             await pageManager.update(false);
             compileCategories();
-            pageManager.scrollToElem(Bookmark.Id);
+            pageManager.scrollToElem(Post.Id);
         }
         else
             renderError("Une erreur est survenue!");
     });
     $('#cancel').on("click", function () {
-        showBookmarks();
+        showPosts();
     });
 }
 function makeFavicon(url, big = false) {
@@ -338,21 +340,21 @@ function makeFavicon(url, big = false) {
     url = "http://www.google.com/s2/favicons?sz=64&domain=" + url;
     return `<div class="${faviconClass}" style="background-image: url('${url}');"></div>`;
 }
-function renderBookmark(Bookmark) {
-    let favicon = makeFavicon(Bookmark.Url);
+function renderPost(Post) {
+    let favicon = makeFavicon(Post.Url);
     return $(`
-     <div class="BookmarkRow" id='${Bookmark.Id}'>
-        <div class="BookmarkContainer noselect">
-            <div class="BookmarkLayout">
-                <div class="Bookmark">
-                    <a href="${Bookmark.Url}" target="_blank"> ${favicon} </a>
-                    <span class="BookmarkTitle">${Bookmark.Title}</span>
+     <div class="PostRow" id='${Post.Id}'>
+        <div class="PostContainer noselect">
+            <div class="PostLayout">
+                <div class="Post">
+                    <a href="${Post.Url}" target="_blank"> ${favicon} </a>
+                    <span class="PostTitle">${Post.Title}</span>
                 </div>
-                <span class="BookmarkCategory">${Bookmark.Category}</span>
+                <span class="PostCategory">${Post.Category}</span>
             </div>
-            <div class="BookmarkCommandPanel">
-                <span class="editCmd cmdIcon fa fa-pencil" editBookmarkId="${Bookmark.Id}" title="Modifier ${Bookmark.Title}"></span>
-                <span class="deleteCmd cmdIcon fa fa-trash" deleteBookmarkId="${Bookmark.Id}" title="Effacer ${Bookmark.Title}"></span>
+            <div class="PostCommandPanel">
+                <span class="editCmd cmdIcon fa fa-pencil" editPostId="${Post.Id}" title="Modifier ${Post.Title}"></span>
+                <span class="deleteCmd cmdIcon fa fa-trash" deletePostId="${Post.Id}" title="Effacer ${Post.Title}"></span>
             </div>
         </div>
     </div>           
